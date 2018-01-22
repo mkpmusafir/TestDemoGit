589325bc731c1a262a6426356573d4d39d9b6a59
mkpmusafir <musaphir2017@gmail.com>
2018-01-22 17:20:33 +0530

    add variable

--- app/src/main/java/bthrust/testdemogit/MainActivity.java
+++ app/src/main/java/bthrust/testdemogit/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String add = "fdasfds";
+        String newAdd = "dfdas fdfjadfdjkf;l dkfd;lsaf";
     }
 }
